3329eb5dbd85b176ff0c5d985d871af732b24492
CrazyBear2002 <levikan2006@gmail.com>
2024-11-12 20:46:49 +0300

    new groups

--- main.py
+++ main.py
@@ -2,10 +2,40 @@ from collections import Counter
 from heroes import heroes_list
 
 
-def find_best_heroes(heroes, required_tokens):
-    best_heroes = []
-    max_tokens = 0
+# def find_best_heroes(heroes, required_tokens):
+#     best_heroes = []
+#     max_tokens = 0
 
+#     required_count = Counter(required_tokens)
+
+#     for hero in heroes:
+#         hero_count = Counter(hero.tokens)
+
+#         matching_tokens = sum(min(hero_count[token], required_count[token]) for token in required_count)
+
+#         if matching_tokens > max_tokens:
+#             max_tokens = matching_tokens
+#             best_heroes = [hero.name]
+#         elif matching_tokens == max_tokens:
+#             best_heroes.append(hero.name)
+
+#     return best_heroes, max_tokens
+
+# def main(heroes_list):
+
+
+#     required_tokens = input("Enter the required tokens separated by a space: ").split(",")
+
+#     required_tokens = [token.strip().capitalize() for token in required_tokens]
+
+#     best_heroes, max_tokens = find_best_heroes(heroes_list, required_tokens)
+
+#     print(f"Best heroes: \n\n{', '.join(best_heroes)} \n\nyou can get: {max_tokens} tokens if you win")
+
+def find_best_heroes(heroes, required_tokens):
+    heroes_with_3_tokens = []
+    heroes_with_2_tokens = []
+    
     required_count = Counter(required_tokens)
 
     for hero in heroes:
@@ -13,24 +43,25 @@ def find_best_heroes(heroes, required_tokens):
 
         matching_tokens = sum(min(hero_count[token], required_count[token]) for token in required_count)
 
-        if matching_tokens > max_tokens:
-            max_tokens = matching_tokens
-            best_heroes = [hero.name]
-        elif matching_tokens == max_tokens:
-            best_heroes.append(hero.name)
+        if matching_tokens == 3:
+            heroes_with_3_tokens.append(hero.name)
+        elif matching_tokens == 2:
+            heroes_with_2_tokens.append(hero.name)
 
-    return best_heroes, max_tokens
+    return heroes_with_3_tokens, heroes_with_2_tokens
+
 
 def main(heroes_list):
-
-
     required_tokens = input("Enter the required tokens separated by a space: ").split(",")
 
     required_tokens = [token.strip().capitalize() for token in required_tokens]
 
-    best_heroes, max_tokens = find_best_heroes(heroes_list, required_tokens)
+    heroes_with_3_tokens, heroes_with_2_tokens = find_best_heroes(heroes_list, required_tokens)
 
-    print(f"Best heroes: \n\n{', '.join(best_heroes)} \n\nyou can get: {max_tokens} tokens if you win")
+    print(f"Heroes with 3 matching tokens: \n\n{', '.join(heroes_with_3_tokens) if heroes_with_3_tokens else 'None'}")
+    print("\n--------------")
+    print(f"Heroes with 2 matching tokens: \n\n{', '.join(heroes_with_2_tokens) if heroes_with_2_tokens else 'None'}")
+    print("\n--------------")
 
 if __name__ == "__main__":
     main(heroes_list)
